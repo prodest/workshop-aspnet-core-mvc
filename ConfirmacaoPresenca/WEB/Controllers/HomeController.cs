d0746f700ac720427eecf080184416ca6adce4cf
Breno Wruck Schneider <brenows@gmail.com>
2017-09-21 15:47:00 -0300

    Criação do esqueleto básico da aplicação.

---
+++ ConfirmacaoPresenca/WEB/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewData["title"] = "Início";
+            return View("PaginaInicial");
+        }
+
+        [HttpGet]
+        public IActionResult Formulario()
+        {
+            ViewData["title"] = "Confirmação de presença";
+            return View();
+        }
+    }
+}
